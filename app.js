ea2e80482376a0e04f96d4b4d9caa4abd4dfa4bf
danil syah <danilsyaharihardjo@gmail.com>
2020-07-19 20:15:24 +0700

    add file views/top.ejs

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// import package framework express
 const express = require('express');
+// import package database mysql
+const mysql = require("mysql");
 const app = express();
+// route asset folder public
+app.use(express.static("public"));
 
 app.get('/', (req, res) => {
-  res.render('hello.ejs');
+  res.render('top.ejs');
 });
 
 app.listen(3000);
