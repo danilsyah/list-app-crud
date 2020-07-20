be7ec08bce57e0a775ca038cd9cccf9a797242e9
danil syah <danilsyaharihardjo@gmail.com>
2020-07-20 13:22:31 +0700

    mod process function update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // import package framework express
-const express = require('express');
+const express = require("express");
 // import package database mysql
 const mysql = require("mysql");
 const app = express();
@@ -7,16 +7,18 @@ const app = express();
 app.use(express.static("public"));
 
 // config request form value
-app.use(express.urlencoded({
-  extended: false,
-}));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 // create object connection database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
-  database: "list_app"
+  database: "list_app",
 });
 
 // check condition database
@@ -28,15 +30,15 @@ connection.connect(function (err) {
   console.log("connection database success");
 });
 
-app.get('/', (req, res) => {
-  res.render('top.ejs');
+app.get("/", (req, res) => {
+  res.render("top.ejs");
 });
 
 app.get("/index", (req, res) => {
   connection.query("SELECT * FROM schedules", (error, results) => {
     console.log(results);
     res.render("index.ejs", {
-      schedules: results
+      schedules: results,
     });
   });
 });
@@ -69,4 +71,26 @@ app.post("/delete/:id", (req, res) => {
   );
 });
 
+app.get("/edit/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM schedules WHERE id = ?",
+    [req.params.id],
+    (error, results) => {
+      res.render("edit.ejs", { item: results[0] });
+    }
+  );
+});
+
+// route process update
+app.post("/update/:id", (req, res) => {
+  connection.query(
+    "UPDATE schedules SET description = ? WHERE id = ?",
+    [req.body.itemDesc, req.params.id],
+    (error, results) => {
+      res.redirect("/index");
+    }
+  );
+});
+
+// port run server
 app.listen(3000);
